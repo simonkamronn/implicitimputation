bf4ae8aada5128d6bbe5b0a5311ab78cdbd790c6
Simon Kamronn <simon@kamronn.dk>
2017-03-28 11:04:57 +0200

    Correct data save path

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -151,6 +151,10 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    dataloader = DataLoader()
+    data = dataloader.load_all()
+    np.save(os.path.join(os.pardir, os.pardir, 'data', 'processed', 'data.npy'), data)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
